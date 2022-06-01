15703e8106479303ae008217325d189882dc47af
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-06-01 22:49:00 +0200

    add solution

--- cache.go
+++ cache.go
@@ -1,23 +1,69 @@
 package cache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
+
+type Data struct {
+	value    string
+	deadline *time.Time
+}
 
 type Cache struct {
+	mu   sync.Mutex
+	data map[string]Data
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		data: map[string]Data{},
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (cache *Cache) Get(key string) (string, bool) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
 
+	if _, ok := cache.data[key]; !ok {
+		return "", false
+	}
+
+	if cache.data[key].deadline != nil && cache.data[key].deadline.Before(time.Now()) {
+		return "", false
+	}
+
+	return cache.data[key].value, true
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) Put(key, value string) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	cache.data[key] = Data{value, nil}
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Keys() []string {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	var keys []string
+	now := time.Now()
+
+	for k, v := range cache.data {
+		if v.deadline != nil && v.deadline.Before(now) {
+			continue
+		}
+
+		keys = append(keys, k)
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	cache.data[key] = Data{value, &deadline}
 }
